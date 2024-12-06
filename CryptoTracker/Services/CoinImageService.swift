f02a7770bde4bae6e99db3224ae3a48fbf761074
joricky91 <jonathanricky91@gmail.com>
2024-12-06 16:44:41 +0700

    feat: save coin image to FileManager

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -12,15 +12,31 @@ import Combine
 class CoinImageService {
     
     @Published var image: UIImage? = nil
+    
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image from File Manager!")
+        } else {
+            downloadCoinImage()
+            print("Downloading image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
@@ -29,9 +45,11 @@ class CoinImageService {
             }
             .sink { completion in
                 NetworkingManager.handleCompletion(completion: completion)
-            } receiveValue: { [weak self] image in
-                self?.image = image
-                self?.imageSubscription?.cancel()
+            } receiveValue: { [weak self] returnedImage in
+                guard let self, let returnedImage else { return }
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: returnedImage, imageName: self.imageName, folderName: self.folderName)
             }
     }
     
